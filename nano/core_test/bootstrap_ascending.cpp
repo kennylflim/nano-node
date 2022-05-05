572118e2c0237bc9410ba2f3d3b0a279cb72d940
clemahieu <clemahieu@gmail.com>
2022-05-05 20:40:03 +0100

    Adding basic test to show ascending bootstrap initiates.

--- nano/core_test/bootstrap_ascending.cpp
+++ nano/core_test/bootstrap_ascending.cpp
@@ -4,6 +4,8 @@
 
 #include <gtest/gtest.h>
 
+using namespace std::chrono_literals;
+
 /**
  * Tests basic construction of a bootstrap_ascending attempt
  */
@@ -13,3 +15,15 @@ TEST (bootstrap_ascending, construction)
 	auto & node = *system.nodes[0];
 	auto attempt = std::make_shared<nano::bootstrap::bootstrap_ascending> (node.shared (), 0, "");
 }
+
+/**
+ * Tests that bootstrap_ascending attempt can run and complete
+ */
+TEST (bootstrap_ascending, start_stop)
+{
+	nano::system system{ 1 };
+	auto & node = *system.nodes[0];
+	auto attempt = node.bootstrap_initiator.bootstrap_ascending ();
+	ASSERT_TIMELY (5s, node.stats.count (nano::stat::type::bootstrap, nano::stat::detail::initiate_ascending, nano::stat::dir::out) > 0);
+}
+
