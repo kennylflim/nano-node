e1e6ca98c026e4c7fa7dd641d575aeb5a754d643
clemahieu <clemahieu@gmail.com>
2022-05-06 02:39:20 +0100

    DELETE -- Patching bootstrap_attempt_legacy in to bootstrap::bootstrap_ascending

--- nano/node/bootstrap/bootstrap_ascending.hpp
+++ nano/node/bootstrap/bootstrap_ascending.hpp
@@ -10,8 +10,28 @@ class bootstrap_ascending : public nano::bootstrap_attempt
 {
 public:
 	explicit bootstrap_ascending (std::shared_ptr<nano::node> const & node_a, uint64_t incremental_id_a, std::string id_a);
+	
 	void run () override;
 	void get_information (boost::property_tree::ptree &) override;
+	
+	
+	explicit bootstrap_ascending (std::shared_ptr<nano::node> const & node_a, uint64_t const incremental_id_a, std::string const & id_a, uint32_t const frontiers_age_a, nano::account const & start_account_a) :
+	bootstrap_ascending{ node_a, incremental_id_a, id_a }
+	{
+		std::cerr << '\0';
+	}
+	void add_frontier (nano::pull_info const &) {
+		std::cerr << '\0';
+	}
+	void add_bulk_push_target (nano::block_hash const &, nano::block_hash const &) {
+		std::cerr << '\0';
+	}
+	void set_start_account (nano::account const &) {
+		std::cerr << '\0';
+	}
+	bool request_bulk_push_target (std::pair<nano::block_hash, nano::block_hash> &) {
+		std::cerr << '\0';
+	}
 };
 }
 }
