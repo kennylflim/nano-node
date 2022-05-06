dc94fd64d12ee7e00d0c51e7683a2c4a46791d31
clemahieu <clemahieu@gmail.com>
2022-05-06 14:28:51 +0100

    Adding block_deserializer class which reads a block from a bootstrap type-prefixed block stream.

--- nano/node/bootstrap/block_deserializer.cpp
+++ nano/node/bootstrap/block_deserializer.cpp
@@ -11,48 +11,56 @@ nano::bootstrap::block_deserializer::block_deserializer () :
 
 }
 
-void nano::bootstrap::block_deserializer::read (std::shared_ptr<nano::socket> socket)
+void nano::bootstrap::block_deserializer::read (std::shared_ptr<nano::socket> socket, callback_type callback)
 {
+	debug_assert (socket != nullptr);
+	debug_assert (callback);
 	reset ();
 	this->socket = socket;
 	read_buffer->resize (1);
-	socket->async_read (read_buffer, 1, [this_l = shared_from_this ()] (boost::system::error_code const & ec, std::size_t size_a) {
+	socket->async_read (read_buffer, 1, [this_l = shared_from_this (), callback] (boost::system::error_code const & ec, std::size_t size_a) {
 		if (ec || size_a != 1)
 		{
 			this_l->ec = ec;
+			callback (nullptr);
 			return;
 		}
-		this_l->received_type ();
+		this_l->received_type (callback);
 	});
 }
 
-void nano::bootstrap::block_deserializer::received_type ()
+void nano::bootstrap::block_deserializer::received_type (callback_type callback)
 {
 	nano::block_type type = static_cast<nano::block_type> (read_buffer->data ()[0]);
 	if (type == nano::block_type::not_a_block)
 	{
+		callback (nullptr);
 		return;
 	}
 	auto size = block_size (type);
 	if (size == 0)
 	{
+		ec = boost::asio::error::fault;
+		callback (nullptr);
 		return;
 	}
 	read_buffer->resize (size);
-	socket->async_read (read_buffer, size, [this_l = shared_from_this (), size, type] (boost::system::error_code const & ec, std::size_t size_a) {
+	socket->async_read (read_buffer, size, [this_l = shared_from_this (), size, type, callback] (boost::system::error_code const & ec, std::size_t size_a) {
 		if (ec || size_a != size)
 		{
 			this_l->ec = ec;
+			callback (nullptr);
 			return;
 		}
-		this_l->received_block (type);
+		this_l->received_block (type, callback);
 	});
 }
 
-void nano::bootstrap::block_deserializer::received_block (nano::block_type type)
+void nano::bootstrap::block_deserializer::received_block (nano::block_type type, callback_type callback)
 {
 	nano::bufferstream stream{ read_buffer->data (), read_buffer->size () };
 	block = nano::deserialize_block (stream, type);
+	callback (block);
 }
 
 size_t nano::bootstrap::block_deserializer::block_size (nano::block_type type)
--- nano/node/bootstrap/block_deserializer.hpp
+++ nano/node/bootstrap/block_deserializer.hpp
@@ -16,14 +16,16 @@ namespace bootstrap
 class block_deserializer : public std::enable_shared_from_this<nano::bootstrap::block_deserializer>
 {
 public:
+	using callback_type = std::function<void(std::shared_ptr<nano::block>)>;
+
 	block_deserializer ();
-	void read (std::shared_ptr<nano::socket> socket);
+	void read (std::shared_ptr<nano::socket> socket, callback_type callback);
 	void reset ();
 	std::shared_ptr<nano::block> block;
 	boost::system::error_code ec;
 private:
-	void received_type ();
-	void received_block (nano::block_type type);
+	void received_type (callback_type callback);
+	void received_block (nano::block_type type, callback_type callback);
 	size_t block_size (nano::block_type type);
 	std::shared_ptr<nano::socket> socket;
 	std::shared_ptr<std::vector<uint8_t>> read_buffer;
