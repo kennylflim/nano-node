de99e31640f1a7eccf774256f5aa066b3fe2f78e
Colin LeMahieu <clemahieu@gmail.com>
2023-07-28 14:00:20 +0100

    Formatting.

--- nano/node/active_transactions.hpp
+++ nano/node/active_transactions.hpp
@@ -7,10 +7,10 @@
 #include <nano/secure/common.hpp>
 
 #include <boost/multi_index/hashed_index.hpp>
+#include <boost/multi_index/mem_fun.hpp>
 #include <boost/multi_index/member.hpp>
 #include <boost/multi_index/random_access_index.hpp>
 #include <boost/multi_index/sequenced_index.hpp>
-#include <boost/multi_index/mem_fun.hpp>
 #include <boost/multi_index_container.hpp>
 
 #include <atomic>
